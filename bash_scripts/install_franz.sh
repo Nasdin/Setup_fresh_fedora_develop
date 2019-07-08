88c76755dfa6852cd272f17a19f192c38f58a960
nasrudin salim <nasrudin.salim.suden@gmail.com>
2019-07-09 00:05:02 +0800

    Install Franz script

---
+++ bash_scripts/install_franz.sh
@@ -0,0 +1,6 @@
+# Download Franz Appimage
+# Edit this line if Franz version changes
+sudo wget --output-document /opt/franz.AppImage "https://github.com/meetfranz/franz/releases/download/v5.1.0/franz-5.1.0-x86_64.AppImage"
+sudo chmod a+x /opt/franz*.AppImage
+echo "alias franz='/opt/franz.AppImage'" >> ~/.bashrc
+

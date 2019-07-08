41f00b3413c95167c6a65ee5a89412b28e3fc263
nasrudin salim <nasrudin.salim.suden@gmail.com>
2019-07-08 23:00:01 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Setup Fresh FedoraOS Computer
+
+## For Development use, easy setup via a single bash script
+
+## Contains my configurations
+	- Vim
+	- JetBrains ToolKit
+	- Vim Configured for Python
+	- Docker
+	- Franz
+	- Kubernetes(minikube)
+	- Deepin
+	- Anaconda
+	- chrome
+	- Git setup and ssh setup
+	- LastPass
---
+++ bash_scripts/configure_git.sh
@@ -0,0 +1,13 @@
+sudo dnf install git
+git config --global user.email "nasrudin.salim.suden@gmail.com"
+git config --global user.name "nasrudin salim"
+ssh-keygen -t rsa -b 4096 -C "nasrudin.salim.suden@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+sudo dnf install xclip
+xclip -sel clip < ~/.ssh/id_rsa.pub
+echo "your ssh key has been pasted into the clipboard"
+echo "Please configure your github/git with the sshkey"
+echo "once you're done, hit any key"
+read -p "Press enter to continue"
+echo "git has been configured"

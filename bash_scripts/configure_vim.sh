e91cdccb215f28aa102c5520aa90acba25c2049a
Nasrudin Salim <nasrudin.salim.suden@gmail.com>
2019-07-18 10:06:57 +0800

    Fixed bug in vim config

--- bash_scripts/configure_vim.sh
+++ bash_scripts/configure_vim.sh
@@ -1,3 +1,5 @@
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
 git clone --depth=1 https://github.com/amix/vimrc.git /opt/vim_runtime
 sh ~/.vim_runtime/install_awesome_parameterized.sh /opt/vim_runtime --all
 

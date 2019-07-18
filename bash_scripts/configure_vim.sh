d3f90ba191c8d7b28272358e19674e56958df6ac
Nasrudin Salim <nasrudin.salim.suden@gmail.com>
2019-07-18 18:17:05 +0800

    Add Tabnine into vim

--- bash_scripts/configure_vim.sh
+++ bash_scripts/configure_vim.sh
@@ -2,4 +2,7 @@ git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 git clone --depth=1 https://github.com/amix/vimrc.git /opt/vim_runtime
 sh ~/.vim_runtime/install_awesome_parameterized.sh /opt/vim_runtime --all
+# Install tabnine
+git clone https://github.com/zxqfl/tabnine-vim ~/.vim_runtime/tabnine
+echo 'set rtp+=~/.vim_runtime/tabnine' >> ~/.vimrc
 
--- bash_scripts_OSX/configure_vim.sh
+++ bash_scripts_OSX/configure_vim.sh
@@ -1,5 +1,8 @@
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 git clone --depth=1 https://github.com/amix/vimrc.git /opt/vim_runtime
-sh /opt/vim_runtime/install_awesome_parameterized.sh /opt/vim_runtime --all
+sh ~/.vim_runtime/install_awesome_parameterized.sh /opt/vim_runtime --all
+# Install tabnine
+git clone https://github.com/zxqfl/tabnine-vim ~/.vim_runtime/tabnine
+echo 'set rtp+=~/.vim_runtime/tabnine' >> ~/.vimrc
 
